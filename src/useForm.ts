de2552f8750354cd889062ff93964186fa133caa
alexbakerdev <alexander.george.baker@googlemail.com>
2020-06-12 17:19:30 +0100

    Fix: unregister updates watches

--- src/useForm.ts
+++ src/useForm.ts
@@ -803,9 +803,10 @@ export function useForm<
     name: FieldName<TFieldValues> | FieldName<TFieldValues>[],
   ): void {
     if (fieldsRef.current) {
-      (isArray(name) ? name : [name]).forEach((fieldName) =>
-        removeFieldEventListenerAndRef(fieldsRef.current[fieldName], true),
-      );
+      (isArray(name) ? name : [name]).forEach((fieldName) => {
+        removeFieldEventListenerAndRef(fieldsRef.current[fieldName], true);
+        renderWatchedInputs(fieldName);
+      });
     }
   }
 
